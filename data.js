1b6d2326d3e066950b59a89cc032d4fe0efa8ecf
Victor Louveira Amarilha <victor_amarilha@hotmail.com>
2023-08-03 13:28:49 -0300

    Login fix

---
+++ data.js
@@ -0,0 +1,20 @@
+const userData = [
+    {
+      username: "usuario1",
+      password: "senha1",
+      chartData: [10, 15, 8, 12, 20],
+      chartColor: "rgba(255, 99, 132, 0.5)",
+      investmentData: [100, 200, 150, 300, 250],
+      returnData: [200, 250, 180, 270, 320],
+      // Add the investment return data for March to August 2023
+      investmentReturnData: [210, 240, 260, 320, 290, 310],
+    },
+    {
+      username: "usuario2",
+      password: "senha2",
+      chartData: [5, 8, 12, 6, 10],
+      chartColor: "rgba(54, 162, 235, 0.5)",
+      investmentData: [50, 100, 80, 120, 90],
+      returnData: [100, 130, 95, 140, 120],
+    },
+  ];
