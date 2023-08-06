748b82e99a555bf1e27d52350c0c0c0eb655c54c
Victor Louveira Amarilha <victor_amarilha@hotmail.com>
2023-08-06 00:52:20 -0300

    Fixed index

--- authguard.js
+++ authguard.js
@@ -4,12 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function checkPermission(){
     firebase.auth().onAuthStateChanged(user => {
-        const username = user.email.split('@')[0];
+        if (user) {
+            const username = user.email.split('@')[0];
             // User is signed in, redirect to the appropriate page
             console.log(username);
             const currentPage = window.location.pathname;
             const allowedPage = "/pages/" + username + ".html";
-        if (user) {
             console.log(currentPage)
             console.log(allowedPage)
             if (!allowedPage.includes(currentPage)){
@@ -17,7 +17,16 @@ function checkPermission(){
             }
         } 
         else{
-            window.location.href = "login.html"
+            if (window.location.href.includes('index.html')) {
+                // This code will only run if the current page is NOT index.html
+                window.location.href = "pages/login.html"; // Replace 'yourFunction()' with the function you want to execute
+            }
+            else if (window.location.href.includes('pages/login.html')){
+
+            }
+            else{
+                window.location.href = "login.html";
+            }
             return;
         }
     });
