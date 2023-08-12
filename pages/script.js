9cc38c32ddfce03b38485405adb8892d19ba9ff1
Victor Louveira Amarilha <victor_amarilha@hotmail.com>
2023-08-12 00:02:26 -0300

    Add Dashboard from AmandaCupolaa

---
+++ pages/script.js
@@ -0,0 +1,103 @@
+// Função para atualizar o número em um elemento HTML
+function updateNumber(elementId, number) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.textContent = "+" + number;
+    }
+}
+
+// Exemplo de uso para atualizar os números (substitua os números de exemplo pelos números desejados)
+updateNumber('applicationsNumber', 3000); // Atualiza "Applications" para 3000
+updateNumber('shortlistedNumber', 6000); // Atualiza "Shortlisted" para 6000
+updateNumber('peopleNumber', 4500); // Atualiza "People" para 4500
+
+
+var chart    = document.getElementById('chart').getContext('2d'),
+    gradient = chart.createLinearGradient(0, 0, 0, 450);
+
+gradient.addColorStop(0, 'rgba(0, 199, 214, 0.32)');
+gradient.addColorStop(0.3, 'rgba(0, 199, 214, 0.1)');
+gradient.addColorStop(1, 'rgba(0, 199, 214, 0)');
+
+
+var data  = {
+    labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October', 'November','December' ],
+    datasets: [{
+			label: 'Applications',
+			backgroundColor: gradient,
+			pointBackgroundColor: '#00c7d6',
+			borderWidth: 1,
+			borderColor: '#0e1a2f',
+			data: [60, 45, 80, 30, 35, 55,25,80,40,50,80,50]
+    }]
+};
+
+var options = {
+	responsive: true,
+	maintainAspectRatio: true,
+	animation: {
+		easing: 'easeInOutQuad',
+		duration: 520
+	},
+	scales: {
+		yAxes: [{
+      ticks: {
+        fontColor: '#5e6a81'
+      },
+			gridLines: {
+				color: 'rgba(200, 200, 200, 0.08)',
+				lineWidth: 1
+			}
+		}],
+    xAxes:[{
+      ticks: {
+        fontColor: '#5e6a81'
+      }
+    }]
+	},
+	elements: {
+		line: {
+			tension: 0.4
+		}
+	},
+	legend: {
+		display: false
+	},
+	point: {
+		backgroundColor: '#00c7d6'
+	},
+	tooltips: {
+		titleFontFamily: 'Poppins',
+		backgroundColor: 'rgba(0,0,0,0.4)',
+		titleFontColor: 'white',
+		caretSize: 5,
+		cornerRadius: 2,
+		xPadding: 10,
+		yPadding: 10
+	}
+};
+
+var chartInstance = new Chart(chart, {
+    type: 'line',
+    data: data,
+		options: options
+});
+
+document.querySelector('.open-right-area').addEventListener('click', function () {
+    document.querySelector('.app-right').classList.add('show');
+});
+
+document.querySelector('.close-right').addEventListener('click', function () {
+    document.querySelector('.app-right').classList.remove('show');
+});
+
+document.querySelector('.menu-button').addEventListener('click', function () {
+    document.querySelector('.app-left').classList.add('show');
+});
+
+document.querySelector('.close-menu').addEventListener('click', function () {
+    document.querySelector('.app-left').classList.remove('show');
+});
+
+
+
