761255abfa8b2b25cb9ce0f811b3b65143e8124c
Victor Louveira Amarilha <victor_amarilha@hotmail.com>
2023-09-10 16:50:14 -0300

    Profile info working

--- username.js
+++ username.js
@@ -1,10 +1,14 @@
 const userNameHeader = document.getElementById('usernameHeader')
 const valueElement = document.getElementById('applicationsNumber');
 const usernameProfile = document.getElementById('profileUsername');
+const usernameInfo = document.getElementById('usernameInfo');
+const mailUser = document.getElementById('mailUser');
 // Verificar o estado de autenticação do usuário
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     const userUid = user.uid;
+    const userMail = user.email;
+    console.log(userMail);
     console.log(userUid)
     // Referência ao documento específico com base no UID do usuário
     const docRef = firebase.firestore().collection('database').doc(userUid);
@@ -17,8 +21,15 @@ firebase.auth().onAuthStateChanged((user) => {
           console.log(currentStatus)
           console.log(currentUser);
           // Atualizar o conteúdo do <span> com o valor obtido
-         userNameHeader.textContent = "Bem vindo, " + currentUser + "!!!";
-         usernameProfile.textContent = currentUser;
+          try{
+            userNameHeader.textContent = "Bem vindo, " + currentUser + "!!!";
+          }catch{
+            usernameProfile.innerHTML = currentUser;
+            usernameInfo.innerHTML += currentUser;
+            mailUser.innerHTML += userMail;
+            
+          }
+         
         } else {
           console.log('O documento não foi encontrado.');
         }
